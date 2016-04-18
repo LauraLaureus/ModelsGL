665a3fffcc3bce2213a642319ed3eb95fd444150
Laura <l4depende@gmail.com>
2016-04-18 08:57:17 +0100

    Initial Commit

---
+++ ModelsFromFile/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ModelsFromFile
+//
+//  Created by Laura del Pino Díaz on 18/4/16.
+//  Copyright © 2016 Laura del Pino Díaz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
